4107607cf7a0e0183d85e7e520595687c5cf0b87
aumpu simanon <aumpu.sim@spumail.net>
2021-09-15 00:06:31 +0700

    edit APP.js

--- App.js
+++ App.js
@@ -3,15 +3,14 @@ import {Text, View, StyleSheet, ActivityIndicator} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { styles } from '../style'
-
+import {styles} from '../style';
 
 import SignInScreen from './src/screen/SignInScreen';
 import SignUpScreen from './src/screen/SignUpScreen';
 import HomeScreen from './src/screen/HomeScreen';
 import googleMap from './src/screen/googleMap';
 import detialCar from './src/screen/detailCar';
-
+// test github
 
 const Stack = createStackNavigator();
 function MyStack() {
@@ -52,13 +51,11 @@ function MyStack() {
 }
 
 const App = () => {
-
   return (
-      <NavigationContainer>
-        <MyStack />
-      </NavigationContainer>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 };
-
 
 export default App;
